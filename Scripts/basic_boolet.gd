1017cb8ba2cca8c5c926a35b8a87ab16a573a401
RoroZoro41 <elza3ieme@gmail.com>
2025-04-01 14:04:49 +0300

    Currently modifying dodge/bow movement

--- Scripts/basic_boolet.gd
+++ Scripts/basic_boolet.gd
@@ -1,4 +1,9 @@
 extends CharacterBody2D
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body != self:
+		queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,49 +1,67 @@
 extends CharacterBody2D
 
+@onready var lulu_sprite: Sprite2D = $LuluSprite
+
 #player speed
-var speed: float = 500
+const speed: float = 500
+var speed_Modifier: float = 1
 var speed_acceleration: float = speed/3
 var speed_deceleration: float = speed/3
+	#multiplication of speed during dashes
+var speed_acceleration_dash_multiplication: float = 4
+var speed_dash_multiplication: float = 4
+	#multiplication of speed during bow draw
+var speed_acceleration_bow_multiplication: float = 0.4
+var speed_bow_multiplication: float = 0.4
 
 #dodging
 @onready var dash_cooldown:Timer = $"Timers/Dash cooldown"
 var dodging_length: float = 0.05
 
 func  _physics_process(_delta: float) -> void:
-	
+	$Label.text = str(speed_acceleration)
+#region Movement
 #PLAYER INPUTS
 	# Player slowly moves up to target speed, then slowly slows down to zero
 	var direction = Input.get_vector("left","right","up","down")
 	if (direction):
-		velocity = velocity.move_toward(direction * speed, speed_acceleration)
+		velocity = velocity.move_toward(direction * (speed * speed_Modifier), speed_acceleration)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, speed_deceleration)
+#endregion
 	if Input.is_action_just_pressed("dodge"): dodge()
-
+#region Shooting
+	if Input.is_action_just_pressed("shoot"): 
+		shoot()
+	if Input.is_action_just_released("shoot"): 
+		speed_Modifier *= 1/speed_acceleration_bow_multiplication
+#endregion
 	move_and_slide()
+
+func shoot():
+	# Mulitplying speed
+	speed_Modifier *= speed_acceleration_bow_multiplication
+	
 
 func dodge():
 # If you moving, or dash cooldown is active, exit function
 	if not dash_cooldown.is_stopped() or velocity == Vector2.ZERO :
 		return
-
+	
 #TEMPORARY LINE, DELETE SOON
-	invincible(0.3)
-
+	invincible(dash_cooldown.time_left)
+	
 	dash_cooldown.start()
-	var original_speed = speed
-	var original_speed_acceleration = speed_acceleration
 	
 	# Mulitplying speed
-	speed *= 4
-	speed_acceleration *= 4
-	
+	speed_Modifier *= speed_dash_multiplication
+	speed_acceleration *= speed_acceleration_dash_multiplication
+	# Wait [dodging_length] amount of time before ending the dash
 	await get_tree().create_timer(dodging_length).timeout
-	speed = original_speed
-	speed_acceleration = original_speed_acceleration
-	
+	speed_Modifier *= speed_dash_multiplication
+	speed_acceleration *= speed_acceleration
 
 func invincible(invincibility_length:float = 0.5):
-	$Sprite2D.self_modulate.a = 0.5
+	lulu_sprite.self_modulate.a = 0.5
 	await get_tree().create_timer(invincibility_length).timeout
-	$Sprite2D.self_modulate.a = 1
+	lulu_sprite.self_modulate.a = 1
